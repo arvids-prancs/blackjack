b7467f6a658f84c3563019bd71022687776679bf
codebrix <codebrix@gmail.com>
2019-10-17 22:05:22 +0300

    17.10.

--- BlackjackGame.js
+++ BlackjackGame.js
@@ -11,22 +11,54 @@ class BlackjackGame {
                 });
             }
         }
+        this.chipsValues = [10, 20, 50, 100, 500];
         this.bank = 1000;
+        this.bet = 0;
         this.gameGrid = document.createElement("div");
         this.gameGrid.id = "gameGrid";
         document.getElementById(container).appendChild(this.gameGrid);
+        this.chipHolderDiv = document.createElement("div");
+        this.chipHolderDiv.id = "chip-holder";
     }
     start() {
         let row1 = document.createElement("div");
         row1.id = "row1";
         this.gameGrid.appendChild(row1);
-        let row2 = document.createElement("div");
-        row2.id = "row2";
-        this.gameGrid.appendChild(row2);
+        this.displayRow2();
         let row3 = document.createElement("div");
         row3.id = "row3";
         this.gameGrid.appendChild(row3);
         this.displayRow4();
+        this.calculateBet();
+    }
+    displayRow2() {
+        let row2 = document.createElement("div");
+        row2.id = "row2";
+        this.gameGrid.appendChild(row2);
+        //left
+        let leftDiv = document.createElement("div");
+        leftDiv.className = "left-div";
+        row2.appendChild(leftDiv);
+
+        //
+        //center
+        //
+        let centerDiv = document.createElement("div");
+        centerDiv.className = "center-div";
+        row2.appendChild(centerDiv);
+        //chipHolderDiv
+        centerDiv.appendChild(this.chipHolderDiv);
+        //bet
+        let betDiv = document.createElement("div");
+        betDiv.id = "bet";
+        centerDiv.appendChild(betDiv);
+
+
+
+        //right
+        let rightDiv = document.createElement("div");
+        rightDiv.className = "right-div";
+        row2.appendChild(rightDiv);
 
     }
     displayRow4() {
@@ -36,12 +68,12 @@ class BlackjackGame {
         //chips
         let chipsContainer = document.createElement("div");
         row4.appendChild(chipsContainer);
-        let chipsValues = [10, 20, 50, 100, 500];
-        for (let i = 0; i < chipsValues.length; i++) {
+
+        for (let i = 0; i < this.chipsValues.length; i++) {
             let chipDiv = document.createElement("img");
-            chipDiv.src = "img/chips/" + chipsValues[i] + ".png";
+            chipDiv.src = "img/chips/" + this.chipsValues[i] + ".png";
             chipDiv.onclick = () => {
-                console.log(chipsValues[i]);
+                this.calculateBet(i);
             };
             chipsContainer.appendChild(chipDiv);
         }
@@ -50,11 +82,33 @@ class BlackjackGame {
         row4.appendChild(bankContainer);
         let bankDiv = document.createElement("div");
         bankDiv.id = "bank";
-        bankDiv.innerHTML = "€ " + this.bank;
         bankContainer.appendChild(bankDiv);
         let bankText = document.createElement("div");
         bankText.innerHTML = "BANKA";
         bankText.className = "bank-text";
         bankContainer.appendChild(bankText);
     }
+    calculateBet(chipbet = 2) {
+        if (this.bank - this.chipsValues[chipbet] >= 0) {
+            this.displayBetAndBank(this.chipsValues[chipbet]);
+            let chip = document.createElement("img");
+            chip.src = "img/chips/" + this.chipsValues[chipbet] + ".png";
+            chip.style = "top:" + this.getRndInteger(-2, 2) + "px;left:" + this.getRndInteger(-2, 2) + "px;"
+            chip.onclick = () => {
+                this.displayBetAndBank(-this.chipsValues[chipbet]);
+                this.chipHolderDiv.removeChild(chip);
+            };
+            this.chipHolderDiv.appendChild(chip);
+        }
+    }
+    displayBetAndBank(value) {
+        this.bet += value;
+        this.bank -= value;
+        document.getElementById("bet").innerHTML = "€ " + this.bet;
+        document.getElementById("bank").innerHTML = "€ " + this.bank;
+    }
+    getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
 }
